51e49e65c88955d5577823e3c4dd234f4743bbbb
ik04346 <bilaal.yadallee@outlook.fr>
2019-03-09 02:53:39 +0100

    Héritage template + début Modèle

---
+++ module/Personnes/Model/Personnes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: yadallee
+ * Date: 09/03/19
+ * Time: 02:43
+ */
+
+namespace Personnes\Model;
+
+
+class Personnes
+{
+
+    private $id;
+    private $nom;
+
+
+
+}

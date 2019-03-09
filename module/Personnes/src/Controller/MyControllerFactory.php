44063923d37c0cf02e4aa29bbef937f2705b942b
ik04346 <bilaal.yadallee@outlook.fr>
2019-03-09 18:49:44 +0100

    Factory pour MyController

---
+++ module/Personnes/src/Controller/MyControllerFactory.php
@@ -0,0 +1,22 @@
+<?php
+namespace Personnes\Controller;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\AbstractFactoryInterface;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Personnes\Controller\MyController;
+
+class MyControllerFactory implements FactoryInterface
+{
+
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {}
+
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $entityManager = $container->get("doctrine.entitymanager.orm_default");
+        return new MyController($entityManager);
+    }
+}
+

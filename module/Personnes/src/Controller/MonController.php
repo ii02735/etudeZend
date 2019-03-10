0966f3f5a29586adf09f53a3ed20117bbefbf2c0
ik04346 <bilaal.yadallee@outlook.fr>
2019-03-10 02:56:43 +0100

    Création d'un controller

---
+++ module/Personnes/src/Controller/MonController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Personnes\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class MonController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        
+    }
+}
+

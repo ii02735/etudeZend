bc7ad4761f76803fda93cb10071865066653ec83
ik04346 <bilaal.yadallee@outlook.fr>
2019-03-10 18:45:26 +0100

    Migration

---
+++ data/Migrations/Version20190310174419.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace \Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190310174419 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE doctrine_migration_versions CHANGE version version VARCHAR(14) NOT NULL');
+        $this->addSql('ALTER TABLE personne CHANGE nom nom VARCHAR(50) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE doctrine_migration_versions CHANGE version version VARCHAR(14) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE personne CHANGE nom nom VARCHAR(100) DEFAULT NULL COLLATE latin1_swedish_ci');
+    }
+}
